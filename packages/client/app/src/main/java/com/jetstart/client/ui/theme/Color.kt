987367c0873f11ab4c2b7f70f5c614e0b415c3ce
Elimihele God's favour <godsfavour1975@gmail.com>
2025-12-20 00:19:16 +0100

    fix: changed the ui for the client

--- packages/client/app/src/main/java/com/jetstart/client/ui/theme/Color.kt
+++ packages/client/app/src/main/java/com/jetstart/client/ui/theme/Color.kt
@@ -2,15 +2,16 @@ package com.jetstart.client.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// JetStart Brand Colors - matching docs theme
+val JetStartPrimary = Color(0xFF25FF79)      // Bright green
+val JetStartSecondary = Color(0xFFB86709)    // Orange/brown
+val JetStartOrange = Color(0xFFCC4C0C)       // Bright orange
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val JetStartBg = Color(0xFF222222)           // Dark background
+val JetStartAltBg = Color(0xFF1E1E1E)        // Darker background
+val JetStartText = Color(0xFFFFFFFF)         // White text
 
-// JetStart Colors
-val JetStartPrimary = Color(0xFF1976D2)
-val JetStartSecondary = Color(0xFF00ACC1)
-val JetStartTertiary = Color(0xFFFF6F00)
+// Utility colors
+val JetStartSurface = Color(0xFF2A2A2A)      // Slightly lighter than bg
+val JetStartOnPrimary = Color(0xFF222222)    // Text on primary color
+val JetStartError = Color(0xFFFF4444)        // Error red
